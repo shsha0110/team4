11494ee29317eed1e49a7800cbe22dde4b2c4292
shsha0110 <shsh0201@snu.ac.kr>
2023-05-26 12:04:17 +0900

    suction cup interaction
    suction cup interaction

---
+++ Assets/CustomXRGrabInteractable.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CustomXRGrabInteractable : XRGrabInteractable
+{
+    public GameObject suctionCupPlane; // the plane to stick to
+    private bool collidingWithPlane = false;
+    private bool isPressed = false;
+    private InputDevice device;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        isPressed = false;
+    }
+
+    private void Start()
+    {
+        // Find the Suction Cup Interaction object by tag
+        suctionCupPlane = GameObject.FindGameObjectWithTag("suctionCupPlane");
+
+        // Get the first available controller
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        if (devices.Count > 0)
+        {
+            device = devices[0];
+        }
+    }
+
+    private void Update()
+    {
+        if (device.isValid)
+        {
+            device.TryGetFeatureValue(CommonUsages.triggerButton, out isPressed);
+            if (isSelected && collidingWithPlane && isPressed)
+            {
+                this.transform.parent = suctionCupPlane.transform;
+                isPressed = false; // optional: release the button after sticking
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == suctionCupPlane)
+        {
+            collidingWithPlane = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == suctionCupPlane)
+        {
+            collidingWithPlane = false;
+        }
+    }
+}
